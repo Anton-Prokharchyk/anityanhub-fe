5c6566f8cd3ebd698157ed1ca64d6916a671a0b3
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2023-12-09 16:59:36 +0300

    added navigation and login

--- src/app/_header.tsx
+++ src/app/_header.tsx
@@ -17,14 +17,22 @@ export default function Header() {
           alt='logo'
         />
         <SearchBar
-          style={{ width: '300px', height: '40px' }}
+          style={{ width: '300px', height: '40px' }} // TODO: add height and width into searchbar props
           placeholder='Search'
           className={styles['search-bar']}
         />
       </div>
       <div className={styles['right-header']}>
-        <nav>NAVIGATION</nav>
-        <div>SIGN IN/UP</div>
+        <nav className={styles['navigation-container']}>
+          <ul className={styles['navigation-list']}>
+            <li className={styles['navigation-list-item']}>Home</li>
+            <li className={styles['navigation-list-item']}>Genre</li>
+            <li className={styles['navigation-list-item']}>Rooms</li>
+            <li className={styles['navigation-list-item']}>Manga</li>
+            <li className={styles['navigation-list-item']}>Ongoing</li>
+          </ul>
+        </nav>
+        <div className={styles['login-container']}>SIGN UP / SIGN IN</div>
       </div>
     </header>
   );
