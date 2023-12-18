3878612902bc731cffaa50dbd4c3a14478f8d67f
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2023-12-18 15:36:32 +0300

    adds ErrorMessageProptypes HTMLSpanElement

--- src/components/error-message/error-message.proptypes.ts
+++ src/components/error-message/error-message.proptypes.ts
@@ -2,8 +2,8 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
 export default interface ErrorMessageProptypes
   extends DetailedHTMLProps<
-    HTMLAttributes<HTMLParagraphElement>,
-    HTMLParagraphElement
+    HTMLAttributes<HTMLParagraphElement | HTMLSpanElement>,
+    HTMLParagraphElement | HTMLSpanElement
   > {
   children?: ReactNode;
   Tag?: 'p' | 'span';
