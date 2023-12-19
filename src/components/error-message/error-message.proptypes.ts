9048f89328163420bac8ac29a7996845fc6d47a9
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2023-12-19 15:16:59 +0300

    fix typography, error-message types

--- src/components/error-message/error-message.proptypes.ts
+++ src/components/error-message/error-message.proptypes.ts
@@ -2,8 +2,8 @@ import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
 export default interface ErrorMessageProptypes
   extends DetailedHTMLProps<
-    HTMLAttributes<HTMLParagraphElement | HTMLSpanElement>,
-    HTMLParagraphElement | HTMLSpanElement
+    HTMLAttributes<HTMLParagraphElement & HTMLSpanElement>,
+    HTMLParagraphElement & HTMLSpanElement
   > {
   children?: ReactNode;
   Tag?: 'p' | 'span';
