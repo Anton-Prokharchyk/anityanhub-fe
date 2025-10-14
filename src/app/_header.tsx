78bafd90cb6278918a0af73a05a370c1582a2c31
Anton-Prokharchyk <134074437+Anton-Prokharchyk@users.noreply.github.com>
2025-10-14 23:53:10 +0300

    ATH-23/feature/main-page (#33)
    * wip
    
    * wip
    
    * wip

---
+++ src/app/_header.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Button, SearchBar, Typography } from 'anityanhub-ui-lib';
+
+import LoginModal from '@/app/components/LoginModal/LoginModal';
+import Logo from '@/../public/ath-main-logo.svg';
+import ProfileModal from '@/app/components/ProfileModal/ProfileModal';
+
+import styles from './header.module.scss';
+
+export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState<boolean>(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
+
+  return (
+    <header className={styles['header-wrapper']}>
+      <div className={styles['top-header']}>
+        <div className={styles['left-header']}>
+          <Logo alt='logo' />
+          <SearchBar
+            style={{ width: '300px', height: '40px' }} // TODO: add height and width into searchbar props
+            placeholder='Search'
+            className={styles['search-bar']}
+          />
+        </div>
+        <div className={styles['right-header']}>
+          <div
+            className={styles['login-container']}
+            onClick={() => setIsLoggedIn(!isLoggedIn)}
+          >
+            {isLoggedIn ? (
+              <div
+                className={styles.profile}
+                style={{ cursor: 'pointer' }}
+                onClick={() => setIsProfileModalOpen(!isProfileModalOpen)}
+              >
+                <div className={styles['profile-info-container']}>
+                  <Typography className={styles.username} Tag='span'>
+                    Username
+                  </Typography>
+                  <Typography className={styles.status} Tag='span'>
+                    Online
+                  </Typography>
+                </div>
+                <div
+                  className={styles.avatar}
+                  style={{
+                    backgroundColor: 'white',
+                    width: '55px',
+                    height: '55px',
+                  }}
+                />
+              </div>
+            ) : (
+              <div className={styles.login}>
+                <Button appearance='none' style={{ padding: '0' }}>
+                  <Link href='https://google.com'>SIGN UP</Link>
+                </Button>
+                /
+                <Button
+                  onClick={() => setIsLoginModalOpen(true)}
+                  appearance='none'
+                  style={{ padding: '0' }}
+                >
+                  SIGN IN
+                </Button>
+              </div>
+            )}
+            {isProfileModalOpen && <ProfileModal />}
+          </div>
+        </div>
+        {isLoginModalOpen && (
+          <LoginModal setIsLoginModalOpen={setIsLoginModalOpen} />
+        )}
+      </div>
+      <div className={styles['bot-header']}>
+        <nav className={styles['navigation-container']}>
+          <ul className={styles['navigation-list']}>
+            <li className={styles['navigation-list-item']}>Home</li>
+            <li className={styles['navigation-list-item']}>Genre</li>
+            <li className={styles['navigation-list-item']}>Rooms</li>
+            <li className={styles['navigation-list-item']}>Manga</li>
+            <li className={styles['navigation-list-item']}>Ongoing</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import cn from 'classnames';
-import 'anityanhub-ui-lib/style.css';
 
 import Header from '@/app/components/_Header/_Header';
 import Footer from '@/app/components/_Footer/_Footer';
 import MainWrapper from '@/app/components/MainWrapper/MainWrapper';
 import MainContentWrapper from '@/app/components/MainContentWrapper/MainContentWrapper';
 
-import './styles/globals.scss';
+import 'anityanhub-ui-lib/style.css';
+import '@/app/styles/globals.scss';
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
